6a032676991bdcbc0db5e1cc4954c54756da20ba
a$ong <fwscool@163.com>
2024-11-15 02:34:05 +0800

    Initial Commit

---
+++ AffirmatMemoryGame/AffirmatMemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  AffirmatMemoryGameApp.swift
+//  AffirmatMemoryGame
+//
+//  Created by 樊万松 on 2024/11/15.
+//
+
+import SwiftUI
+
+@main
+struct AffirmatMemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AffirmatMemoryGameTests/AffirmatMemoryGameTests.swift
@@ -0,0 +1,16 @@
+//
+//  AffirmatMemoryGameTests.swift
+//  AffirmatMemoryGameTests
+//
+//  Created by 樊万松 on 2024/11/15.
+//
+
+import Testing
+
+struct AffirmatMemoryGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
